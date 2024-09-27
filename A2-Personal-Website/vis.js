e4cdeecc5afe5ddc74caf0c56dc05344bccca82d
JKont2123 <jkontulahti@gmail.com>
2024-09-26 22:27:48 -0700

    Add Completed Assignment 2 Website

--- A2-Personal-Website/vis.js
+++ A2-Personal-Website/vis.js
@@ -1,10 +1,32 @@
 // Add your SVG drawing logic here
 const svg = document.getElementById('vis');
 
-// Example: Draw a simple circle in the SVG
-const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-circle.setAttribute('cx', '400');
-circle.setAttribute('cy', '250');
-circle.setAttribute('r', '50');
-circle.setAttribute('fill', 'blue');
-svg.appendChild(circle);
+const circleCount = 1000;
+
+function createCircle(cx, cy, r) {
+    const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    
+    // Set attributes
+    circle.setAttribute('cx', cx);
+    circle.setAttribute('cy', cy);
+    circle.setAttribute('r', r);
+    
+    // Append line to SVG
+    return circle
+}
+
+// Function to generate random coordinates within the SVG canvas
+function getRandomPosition(width, height, padding = 10) {
+    const x = Math.random() * (width - padding) + padding;
+    const y = Math.random() * (height - padding) + padding;
+    return { x, y };
+}
+
+for (let i = 0; i < circleCount; i++){
+    const {x: cx, y: cy } = getRandomPosition(svg.clientWidth, svg.clientHeight);
+    const r = Math.random() * (12- 1) + 1;
+
+    let circle = createCircle(cx, cy, r);
+    svg.appendChild(circle);
+}
+
